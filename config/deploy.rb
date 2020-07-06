fb7e3ed4ebc756549a3cbf1c6813ad19209e14c0
Takeshita Naoki <Naoki@Takeshita-no-MacBook-Pro.local>
2020-07-06 22:46:46 +0900

    add puma conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,17 @@ set :rbenv_ruby, '2.7.1'
 set :rbenv_type, :user # or :system
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
 
 set :application, "myapp"
 set :repo_url, "git@github.com:naokit1030/myapp.git"
