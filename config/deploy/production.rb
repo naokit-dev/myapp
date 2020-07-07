d3f137dcb34b82d0149639a7957499804ce0bde2
Takeshita Naoki <Naoki@Takeshita-no-MacBook-Pro.local>
2020-07-08 00:26:41 +0900

    fix circleci conf

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ server '54.238.15.249',
    ssh_options: {
        port: 22,
        user: "sample_app", # overrides user setting above
-       keys: %w(~/.ssh/sample_app_key_rsa),
+       keys: %w(~/.ssh/sample_app_key_rsa21fede927a9662bd29b93899aae2ae6f),
        forward_agent: true
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
