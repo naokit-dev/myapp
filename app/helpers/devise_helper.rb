4764f72e80812f43a8bfc5f8369a192dbf3776d4
Takeshita Naoki <Naoki@Takeshita-no-MacBook-Pro.local>
2020-07-08 15:33:31 +0900

    create User model

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,14 @@
+module DeviseHelper
+  
+  def bootstrap_alert(msg_type)
+    case msg_type
+    when "alert"
+      "warning"
+    when "notice"
+      "success"
+    when "error"
+      "danger"
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+  resources :notes
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
