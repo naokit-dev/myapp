6eb51857b4d17d68aaa1ed7d8defc2949591447f
Takeshita Naoki <Naoki@Takeshita-no-MacBook-Pro.local>
2020-07-09 14:43:17 +0900

    create editor interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :articles
   devise_for :users
   resources :users
-  resources :notes
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get '/notes/new', to: 'notes#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
