9df1f704fb458450eb1f772edb415212114a3a5a
Takeshita Naoki <Naoki@Takeshita-no-MacBook-Pro.local>
2020-07-09 17:03:39 +0900

    create article model

--- app/models/note.rb
+++
@@ -1,8 +0,0 @@
-class Note < ApplicationRecord
-    belongs_to :user
-    default_scope -> { order(created_at: :desc) }
-    validates :user_id, presence: true
-    validates :title, presence: true, length: { maximum: 140 }
-    validates :content, presece: true
-    
-end
