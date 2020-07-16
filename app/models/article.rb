3ea1fdbfd5802c3fb5c1f8a84e1249c6ddfb0173
naokit1030 <takeshita.na@gmail.com>
2020-07-17 01:52:08 +0900

    add guest user function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,13 @@
 class Article < ApplicationRecord
   belongs_to :user 
+  require 'hashie'
+
+  def self.create_initial_articles
+    articles = Hashie::Mash.load("app/initial_articles.yml")
+    puts articles
+    articles.each do |article|
+      self.new(title: article.title, content: article.content)
+    end
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles, dependent: :destroy
+
+  def self.guest
+    find_or_create_by(email: "test_user@mail.com") do |user|
+      user.username = "Guest_user"
+      user.password = 123456
+    end
+  end
+
 end
