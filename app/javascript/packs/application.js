18f62a604270720cff0268519ca14ff9e2cd6810
naokit1030 <takeshita.na@gmail.com>
2020-07-19 10:51:25 +0900

    small fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@ require("@fortawesome/fontawesome-free");
 
 import '../stylesheets/application'
 
-fontawesome.dom.i2svg()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
