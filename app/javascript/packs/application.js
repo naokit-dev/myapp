ce96f7be4ee12d428aa6636924c76d49defff012
naokit1030 <takeshita.na@gmail.com>
2020-07-26 15:18:03 +0900

    fix webpacker environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,52 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("@fortawesome/fontawesome-free")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("@fortawesome/fontawesome-free");
 
-import 'bootstrap'
-import '../stylesheets/application'
+import "bootstrap";
+import '@fortawesome/fontawesome-free/js/all'
+import "../stylesheets/application";
+import ClipboardJS from "clipboard";
+import marked from 'marked';
+import hljs from 'highlight.js';
 
+$(function () {
+    // if document is ready
+    alert("jQuery is ready.");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var clipboard = new ClipboardJS('#btn-copy');
+    clipboard.on('success', function (e) {
+        console.log(e);
+    });
+    clipboard.on('error', function (e) {
+        console.log(e);
+    });
+
+    new ClipboardJS('#btn-copy-modal', {
+        container: document.getElementById('modal_initial')
+    });
+}, false)
+
+document.addEventListener("DOMContentLoaded", function () {
+    marked.setOptions({
+        langPrefix: '',
+        highlight: function (code, lang) {
+            return hljs.highlightAuto(code, [lang]).value
+        }
+    });
+})
+
+document.addEventListener("turbolinks:load", function () {
+    $(window).on('load', function () {
+        $('#modal_initial').modal();
+    });
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,15 @@
 const {
-  environment
+    environment
 } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 
 environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
-  })
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        Popper: ['popper.js', 'default']
+    })
 )
 
 module.exports = environment
